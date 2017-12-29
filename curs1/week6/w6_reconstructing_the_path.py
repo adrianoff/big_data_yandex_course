797811bca2707a2ea0005aa8fb11f3b6ee46dde8
adrianoff <adrianov87@gmail.com>
2017-12-29 21:33:43 +0300

    twitter

--- curs1/week6/w6_reconstructing_the_path.py
+++ curs1/week6/w6_reconstructing_the_path.py
@@ -1,4 +1,5 @@
 from pyspark import SparkConf, SparkContext
+
 sc = SparkContext(conf=SparkConf().setAppName("MyApp").setMaster("local"))
 
 
@@ -16,8 +17,9 @@ def complete(item):
     v, old_d, new_d = item[0], item[1][0], item[1][1]
     return (v, old_d if old_d is not None else new_d)
 
-n = 1  # number of partitions
-edges = sc.textFile("./twitter_sample.txt")
+
+n = 5  # number of partitions
+edges = sc.textFile("./twitter_sample_short.txt")
 example = edges.take(1)
 edges = edges.map(parse_edge).cache()
 example = edges.take(1)
@@ -29,12 +31,21 @@ x = 12
 d = 0
 distances = sc.parallelize([(x, d)]).partitionBy(n)
 while True:
-  candidates = distances.join(forward_edges, n).map(step)
-  new_distances = distances.fullOuterJoin(candidates, n).map(complete, True).persist()
-  count = new_distances.filter(lambda i: i[1] == d + 1).count()
-  if count > 0:
-    d += 1
-    distances = new_distances
-    print("d = ", d, "count = ", count)
-  else:
-    break
+    candidates = distances.join(forward_edges, n)
+    example = candidates.collect()
+    candidates = candidates.map(step)
+    example = candidates.collect()
+
+    new_distances = distances.fullOuterJoin(candidates, n)
+    example = new_distances.collect()
+    new_distances = new_distances.map(complete, True).persist()
+    example = new_distances.collect()
+
+    count = new_distances.filter(lambda i: i[1] == d + 1).count()
+    if count > 0:
+        d += 1
+        distances = new_distances
+        example = distances.collect()
+        print("d = ", d, "count = ", count)
+    else:
+        break
