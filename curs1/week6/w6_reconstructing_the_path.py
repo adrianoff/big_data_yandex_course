7930ddc8ca7352c6ef3ed7ebf10993b4f0f12912
adrianoff <adrianov87@gmail.com>
2017-12-29 18:37:33 +0300

    twitter sample

---
+++ curs1/week6/w6_reconstructing_the_path.py
@@ -0,0 +1,40 @@
+from pyspark import SparkConf, SparkContext
+sc = SparkContext(conf=SparkConf().setAppName("MyApp").setMaster("local"))
+
+
+def parse_edge(s):
+    user, follower = s.split("\t")
+    return (int(user), int(follower))
+
+
+def step(item):
+    prev_v, prev_d, next_v = item[0], item[1][0], item[1][1]
+    return (next_v, prev_d + 1)
+
+
+def complete(item):
+    v, old_d, new_d = item[0], item[1][0], item[1][1]
+    return (v, old_d if old_d is not None else new_d)
+
+n = 1  # number of partitions
+edges = sc.textFile("./twitter_sample.txt")
+example = edges.take(1)
+edges = edges.map(parse_edge).cache()
+example = edges.take(1)
+
+forward_edges = edges.map(lambda e: (e[1], e[0])).partitionBy(n).persist()
+example = forward_edges.take(1)
+
+x = 12
+d = 0
+distances = sc.parallelize([(x, d)]).partitionBy(n)
+while True:
+  candidates = distances.join(forward_edges, n).map(step)
+  new_distances = distances.fullOuterJoin(candidates, n).map(complete, True).persist()
+  count = new_distances.filter(lambda i: i[1] == d + 1).count()
+  if count > 0:
+    d += 1
+    distances = new_distances
+    print("d = ", d, "count = ", count)
+  else:
+    break
